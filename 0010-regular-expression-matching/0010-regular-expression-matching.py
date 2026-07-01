fed3cfbd0eb604122d5814c9648394e53db05540
Chandrakant Taram <chandrakanttaram11@gmail.com>
2026-07-01 08:50:27 +0530

    Time: 7 ms (84.03%), Space: 12.8 MB (16.27%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.py
+++ 0010-regular-expression-matching/0010-regular-expression-matching.py
@@ -1,21 +1,22 @@
 class Solution:
     def isMatch(self, s, p):
-        m, n = len(s), len(p)
-        dp = [[False] * (n + 1) for _ in range(m + 1)]
+        memo = {}
         
-        dp[0][0] = True
-        
-        for j in range(2, n + 1):
-            if p[j-1] == '*':
-                dp[0][j] = dp[0][j-2]
+        def dfs(i, j):
+            if (i, j) in memo:
+                return memo[(i, j)]
                 
-        for i in range(1, m + 1):
-            for j in range(1, n + 1):
-                if p[j-1] == s[i-1] or p[j-1] == '.':
-                    dp[i][j] = dp[i-1][j-1]
-                elif p[j-1] == '*':
-                    dp[i][j] = dp[i][j-2] or (
-                        dp[i-1][j] and (p[j-2] == s[i-1] or p[j-2] == '.')
-                    )
-                    
-        return dp[m][n]
+            if j == len(p):
+                return i == len(s)
+            
+            match = i < len(s) and (s[i] == p[j] or p[j] == '.')
+            
+            if j + 1 < len(p) and p[j+1] == '*':
+                ans = dfs(i, j + 2) or (match and dfs(i + 1, j))
+            else:
+                ans = match and dfs(i + 1, j + 1)
+                
+            memo[(i, j)] = ans
+            return ans
+            
+        return dfs(0, 0)
